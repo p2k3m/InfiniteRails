ed59bfaa8c874c3cdba6b14fdae7448c3ed02dfa
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-19 12:03:12 +0530

    chore: refresh asset digests

--- script.js
+++ script.js
@@ -18349,13 +18349,13 @@
   }
 
   const THREE_SCRIPT_URL = (() => {
-    const candidates = createAssetUrlCandidates('vendor/three.min.js?v=030c75d4e909.abcdef123456', {
+    const candidates = createAssetUrlCandidates('vendor/three.min.js?v=030c75d4e909.7d592392cf59', {
       preloadedSelector: 'script[data-preload-three]',
     });
     return candidates.length ? candidates[0] : null;
   })();
   const GLTF_LOADER_URL = (() => {
-    const candidates = createAssetUrlCandidates('vendor/GLTFLoader.js?v=0e92b0589a2a.abcdef123456');
+    const candidates = createAssetUrlCandidates('vendor/GLTFLoader.js?v=0e92b0589a2a.7d592392cf59');
     return candidates.length ? candidates[0] : null;
   })();
   const TEST_DRIVER_SCRIPT_URL = (() => {
