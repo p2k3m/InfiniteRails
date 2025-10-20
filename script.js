f01176ad1aa0bb6fa70fa050bf5b5030cfe28d7f
Pushkar Mishra <55252939+p2k3m@users.noreply.github.com>
2025-10-20 19:06:18 +0530

    Restore SimpleExperience bundle

--- script.js
+++ script.js
@@ -20099,13 +20099,13 @@
   }
 
   const THREE_SCRIPT_URL = (() => {
-    const candidates = createAssetUrlCandidates('vendor/three.min.js?v=030c75d4e909.08e8c830775b-dirty', {
+    const candidates = createAssetUrlCandidates('vendor/three.min.js?v=030c75d4e909.5aef7b7ecb7a-dirty', {
       preloadedSelector: 'script[data-preload-three]',
     });
     return candidates.length ? candidates[0] : null;
   })();
   const GLTF_LOADER_URL = (() => {
-    const candidates = createAssetUrlCandidates('vendor/GLTFLoader.js?v=0e92b0589a2a.08e8c830775b-dirty');
+    const candidates = createAssetUrlCandidates('vendor/GLTFLoader.js?v=0e92b0589a2a.5aef7b7ecb7a-dirty');
     return candidates.length ? candidates[0] : null;
   })();
   const TEST_DRIVER_SCRIPT_URL = (() => {
